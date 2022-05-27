9622b6f9e340c86bae5895f12330ff7ec39cb766
SamuelAryaPermana <92012924+SamuelAryaPermana@users.noreply.github.com>
2022-05-27 15:49:19 +0700

    Update logout.php

--- mainmenu/logout.php
+++ mainmenu/logout.php
@@ -11,12 +11,10 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-
     <title>Log Out</title>
   </head>
   <body>
     <h1>You are Loged out!</h1>
     <a href="../index.html" class="btn">Back to Landing Page</a>
-
   </body>
 </html>
