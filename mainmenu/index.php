b630317e3a504460a46651b0f122e4123f2e3721
SamuelAryaPermana <92012924+SamuelAryaPermana@users.noreply.github.com>
2022-05-27 15:48:53 +0700

    Update index.php

--- mainmenu/index.php
+++ mainmenu/index.php
@@ -3,9 +3,6 @@
 <?php
   include "rumus.php";
   session_start();
-  // if(empty($_SESSION['akses'])){
-  //   echo " mohon login terlebih dahulu";
-  // }
 ?>
 <html lang="en">
   <head>
@@ -20,7 +17,6 @@
     <title>Hello, world!</title>
   </head>
   <body>
-    
     <!-- navbar -->
     <nav class="navbar navbar-expand-lg navbar-light">
         <div class="container">
@@ -41,13 +37,13 @@
         <div class="container">
           <h1 class="display-4">Hello, <span><?php echo $_SESSION['akses'] ?></span>! </h1>
           <h5 class="lead">Let's check your Body Mass Index, Daily Calories requirement,
-            <br> and we will give you good food for you!</h5>
+            <br> and we will give you good advices for you!</h5>
         </div>
       </div>
       <!-- AKhir Jumbotron -->
       <div class="proses">
         <!-- form -->
-        <form class="justify-content-center " action="" method="post">
+        <form class="justify-content-center " method="post">
           <div class=" form">
           <div class="ilusdokter">
             <img src="../asset/dokter.png" alt="">
@@ -92,19 +88,19 @@
           $berat=@$_POST['berat'];
           $tinggi=@$_POST['tinggi'];
 
-          $bulk = array('1. Calories surplus everyday','2. Do some weightlifting workout','3. Dont forget to rest');
-          $cut =array('1. Reduce food portions','2. Eat more vegetables and fruit','3. Cardio exercises');
-          $maintain=array('1. Dont skip breakfast','2. Eat vegetables and foods that contain protein in small portions','3. Light exercise');
           $pengguna = new rumus;
           $bmi = $pengguna ->hitungBMI($berat,$tinggi);
           $kalori = $pengguna ->hitungKal($gender,$usia,$berat,$tinggi);
           $surplus = $kalori + 300;
           $defisit = $kalori - 200;
+          $bulk = array('1. Calories surplus everyday','2. Do some weightlifting workout','3. Dont forget to rest');
+          $cut =array('1. Reduce food portions','2. Eat more vegetables and fruit','3. Cardio exercises');
+          $maintain=array('1. Dont skip breakfast','2. Eat vegetables and foods that contain protein in normal portions','3. Light exercise');
           ?>
           <!-- result -->
           <div class="result justify-content-center">
             <div class="result-desc">
-              <?php if($kalori ==0): ?>
+              <?php if($kalori == 0): ?>
                 <h5 id="be4-isi">Please fill the form</h5>
               <?php endif ?>
               <?php if ($bmi <18.5 and $bmi !=0):?>
@@ -123,14 +119,14 @@
               <?php endif ?>
               <?php if ($bmi>=23 and $bmi<=29.9):?>
                 <h5>Body Mass Index: <?php echo "$bmi" ?></h5>
-                <h6>Your weight is over than the normal range.<br> You need <?php echo "$defisit" ?><br>.. calories to lose your current weight. <br> <br> Follow tips below:</h6>
+                <h6>Your weight is over than the normal range.<br> You need <?php echo "$defisit" ?><br> calories to lose your current weight. <br> <br> Follow tips below:</h6>
                 <?php foreach($cut as $value):?>
                   <p><?php echo $value ?></p>
                 <?php endforeach ?>
               <?php endif ?>
               <?php if ($bmi >30):?>
                 <h5>Body Mass Index: <?php echo "$bmi" ?></h5>
-                <h6>Your weight is at the obesity level.<br>  You need <?php echo "$defisit" ?><br>.. calories to lose your current weight. <br> <br> Follow tips below:</h6>
+                <h6>Your weight is at the obesity level.<br>  You need <?php echo "$defisit" ?><br>calories to lose your current weight. <br> <br> Follow tips below:</h6>
                 <?php foreach($cut as $value):?>
                   <p><?php echo $value ?></p>
                 <?php endforeach ?>
@@ -139,9 +135,6 @@
           </div>
         </div>
       </form>
-
       </div>
-
-    <!-- Saran -->
   </body>
 </html>
