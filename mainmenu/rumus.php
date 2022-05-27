b37944f5b6370b0ff8cc4de25dbf333b6ad46bd7
SamuelAryaPermana <92012924+SamuelAryaPermana@users.noreply.github.com>
2022-05-27 22:33:37 +0700

    Update rumus.php

--- mainmenu/rumus.php
+++ mainmenu/rumus.php
@@ -9,7 +9,7 @@ class rumus{
     public function hitungKal($gender,$usia,$berat,$tinggi){
         switch ($gender) {
             case 'male':
-                $kalori=655+(9.6*$berat)+(1.8*$tinggi)-(4.7*$usia);
+                $kalori=655+(13.7*$berat)+(5*$tinggi)-(6.8*$usia);
                 return $kalori;
                 break;
             case 'female':
